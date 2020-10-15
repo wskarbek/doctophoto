8a48d352ffecfbf18ddd781a6ad07bca73179fb1
Wojciech Skarbek <woj.skarbek@gmial.com>
2020-10-15 18:33:27 +0200

    Doctor, Examination and Examination tables added

--- doctophoto/settings.py
+++ doctophoto/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6b#aoxgua@u+a4$-)_i+3*07ip$n56+w7lx(+*us0fiz12xqgo'
+SECRET_KEY = db.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dtp.apps.DtpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- dtp/models.py
+++ dtp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Examination(models.Model):
+    title = models.CharField(max_length=30)
+    pub_date = models.DateTimeField('date published')
+    doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE)
+
+
+class ExaminationPhoto(models.Model):
+    image = models.CharField(max_length=200)
