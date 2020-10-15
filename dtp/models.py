23652f824aa2ac6948acb4bd6c0df2e6b4966992
Wojciech Skarbek <woj.skarbek@gmial.com>
2020-10-15 19:06:24 +0200

    __str__ methods added to models.py

--- dtp/models.py
+++ dtp/models.py
@@ -5,12 +5,21 @@ from django.contrib.auth.models import User
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.get_full_name()
+
 
 class Examination(models.Model):
     title = models.CharField(max_length=30)
     pub_date = models.DateTimeField('date published')
     doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class ExaminationPhoto(models.Model):
     image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.image
