41aea38bf8503c912bc022ccebdc7592212e7ced
Martyna <matytynka@o2.pl>
2020-11-08 20:41:51 +0100

    added home page

--- doctophoto/settings.py
+++ doctophoto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'doctophoto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'dtp/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
