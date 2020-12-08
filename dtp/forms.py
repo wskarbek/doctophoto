feeb752265d7e6a689a712a5838a1b8e5b9eaab0
Wojciech Skarbek <woj.skarbek@gmail.com>
2020-12-08 15:31:08 +0100

    registration form fix

--- dtp/forms.py
+++ dtp/forms.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
 
 class LoginForm(forms.ModelForm):
     class Meta:
